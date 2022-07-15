8431e2bf3128cf091516c6d25deed2a087fd5101
ini234 <saidemilio151@gmail.com>
2022-07-15 00:14:28 -0500

    k

--- Test/tablero/funcionalidad.js
+++ Test/tablero/funcionalidad.js
@@ -47,6 +47,39 @@ function movimiento(){
         }   
     }
   }
+  const todos = JSON.parse(localStorage.getItem('todos'))||[];
+  const render=()=>{
+      const todolist = document.getElementById('todolist');
+      const todostemplate=todos.map(t=>{
+          return "<li>"+t+"</li>";
+      })
+      todolist.innerHTML=todostemplate.join('')
+      console.log(todostemplate)
+      const selectodo= document.querySelectorAll('#todolist li')
+      selectodo.forEach((selectodo,i)=>{
+      selectodo.addEventListener('click',()=>{
+          selectodo.parentNode.removeChild(selectodo);
+          todos.splice(i,1);
+          render();
+        }) 
+      })
+  }
+  render();
+  window.onload=()=>{
+  const form=document.getElementById('formis');
+  form.onsubmit =(jass) => {
+      jass.preventDefault(); 
+      const todo=document.getElementById('todo');   
+      const todotext=todo.value
+      todo.value='';
+      todos.push(todotext);
+      const todosstring= JSON.stringify(todos);
+      localStorage.setItem('todos',todosstring)
+     render();
+  }
+  }
+  
+  
 
       
       
