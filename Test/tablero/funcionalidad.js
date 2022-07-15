5a16676b37dab704405ed54e55d927f28fafd5e6
ini234 <saidemilio151@gmail.com>
2022-07-14 20:35:58 -0500

    apoyo

---
+++ Test/tablero/funcionalidad.js
@@ -0,0 +1,13 @@
+const abrir=document.getElementById("b1");
+const divisor=document.getElementById("agregaejercicio");
+const cerrar=document.getElementById("img3");
+
+
+abrir.addEventListener("click",()=>{
+    divisor.style.display='none';
+alert("sisis")
+    
+})
+
+      
+      
