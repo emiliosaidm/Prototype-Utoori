b2b5a76bf2bb2fdac96ec5c33e4040b68d0839be
ini234 <saidemilio151@gmail.com>
2022-07-14 21:13:59 -0500

    Second to last

--- Test/tablero/funcionalidad.js
+++ Test/tablero/funcionalidad.js
@@ -1,13 +1,52 @@
-const abrir=document.getElementById("b1");
+const abrir=document.getElementById("exapop");
 const divisor=document.getElementById("agregaejercicio");
 const cerrar=document.getElementById("img3");
 
-
-abrir.addEventListener("click",()=>{
+function uru(){
     divisor.style.display='none';
-alert("sisis")
+}
+abrir.addEventListener("click",()=>{
+    divisor.style.display='';
     
 })
+cerrar.addEventListener("click",()=>{
+    divisor.style.display='none';
+    
+})
+
+function movimiento(){
+    var pbarra=document.getElementById('barra');
+    var progreso= 0;
+    var tiempo= setInterval(mov,10);
+    function mov(){
+      if(progreso>=100){
+        clearInterval(tiempo);
+      }
+      else{
+        progreso++;
+        pbarra.style.width=progreso+'%';
+        pbarra.innerHTML=progreso*1+'%';
+        }   
+    }
+  }
+  
+  setTimeout(regreso,1000);
+  
+  function regreso(){
+    var pbarra=document.getElementById('barra');
+    var progreso= 100;
+    var tiempo= setInterval(mov,10);
+    function mov(){
+      if(progreso==0){
+        clearInterval(tiempo);
+      }
+      else{
+        progreso--;
+        pbarra.style.width=progreso+'%';
+        pbarra.innerHTML=progreso*1+'%';
+        }   
+    }
+  }
 
       
       
